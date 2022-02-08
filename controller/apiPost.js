c57fb21dbd609cde5b00931121dc72c9f442c2f7
Flo-314 <julianflo1916@gmail.com>
2022-02-08 15:00:45 -0300

    feat payment post added

--- controller/apiPost.js
+++ controller/apiPost.js
@@ -1,16 +1,33 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-undef */
 const Category = require('../models/category');
 const User = require('../models/user');
 const Payment = require('../models/payment');
 
-exports.category = async (req, res, next) => {
+exports.category = async (req, res) => {
   const { name, _id, target } = req.body;
+  // crea nueva categoria
   const newCategory = await new Category({ name });
   await newCategory.save();
+
   const user = await User.findById({ _id });
+  // busca donde poner categoria, income outcome.
   user[target].push(newCategory._id);
-  console.log(user);
   await user.save();
   res.json({ msg: 'category created' });
 };
-exports.payment = async (req, res, next) => {};
+
+exports.payment = async (req, res) => {
+  const { ammount, toFrom, isMonthly, _id } = req.body;
+  // crea nueva categoria
+
+  const newPayment = await new Payment({ ammount, toFrom, isMonthly });
+  await newPayment.save();
+
+  const user = await Category.findById({ _id });
+  // busca donde poner categoria, income outcome.
+  user.payments.push(newPayment._id);
+  await user.save();
+  res.json({ msg: 'category created' });
+};
 exports.login = async (req, res, next) => {};
--- models/payment.js
+++ models/payment.js
@@ -5,7 +5,7 @@ const { Schema } = mongoose;
 const payment = new Schema({
   ammount: { type: String, required: true },
   toFrom: { type: String, required: true },
-  isMontly: { type: Boolean, required: true },
+  isMonthly: { type: Boolean, required: true },
 });
 
 module.exports = mongoose.model('payment', payment);
