82bce4e2e02e3a2ff166502105d01b08a6a8b267
Flo-314 <julianflo1916@gmail.com>
2022-02-08 14:48:23 -0300

    feat: category post added

--- controller/apiPost.js
+++ controller/apiPost.js
@@ -1,6 +1,16 @@
 const Category = require('../models/category');
+const User = require('../models/user');
 const Payment = require('../models/payment');
 
-exports.category = async (req, res, next) => {};
+exports.category = async (req, res, next) => {
+  const { name, _id, target } = req.body;
+  const newCategory = await new Category({ name });
+  await newCategory.save();
+  const user = await User.findById({ _id });
+  user[target].push(newCategory._id);
+  console.log(user);
+  await user.save();
+  res.json({ msg: 'category created' });
+};
 exports.payment = async (req, res, next) => {};
 exports.login = async (req, res, next) => {};
