ded2ff40dcf7b03b6f0f520aa70ccdd18149c2f8
Flo-314 <julianflo1916@gmail.com>
2022-02-08 15:08:02 -0300

    feat: category,payment update added

--- controller/apiUpdate.js
+++ controller/apiUpdate.js
@@ -1,5 +1,17 @@
 const Category = require('../models/category');
 const Payment = require('../models/payment');
 
-exports.category = async (req, res, next) => {};
-exports.payment = async (req, res, next) => {};
+exports.category = async (req, res) => {
+  const { name, _id } = req.body;
+  const category = await Category.findById({ _id });
+  category.name = name;
+  await category.save();
+  res.json({ msg: 'category name edited' });
+};
+exports.payment = async (req, res) => {
+  const { ammount, toFrom, isMonthly, _id } = req.body;
+  let payment = await Payment.findById({ _id });
+  payment = { ammount, toFrom, isMonthly };
+  await payment.save();
+  res.json({ msg: 'payment name edited' });
+};
